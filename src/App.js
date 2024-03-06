896180f38716f30d9a27a29af10853b65dbdc342
aleramirezsj <aleramirezsj@gmail.com>
2024-03-06 17:58:04 -0300

    Avances hasta el inicio del video 3 (Componentes de TODO Machine)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <TodoItem />
+      <TodoCount completed={3}  total={5}/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +25,34 @@ function App() {
     </div>
   );
 }
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Elemento de prueba</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+function TodoCount({completed, total}) {
+  return (
+    <h1>
+      Completaste {completed} de {total} TODOs
+    </h1>
+  );
+}
+
+<App>
+  <TodoCounter />
+  <TodoSearch />
+  
+  <TodoList>
+    <TodoItem />
+  </TodoList>
+
+  <CreateTodoButton />
+</App>
 
 export default App;
